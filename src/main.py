ad644e4a31f7475c58a35d4d3172bc4d6231d843
James Page <james@ecreationtech.co.uk>
2026-02-11 01:03:20 +0000

    Removed copying of .env file into image

--- src/main.py
+++ src/main.py
@@ -8,9 +8,6 @@ import os
 def main() -> None:
     load_dotenv()
 
-    bucket_name = os.getenv("BUCKET") 
-    print(f"Bucket name is: {bucket_name}")
-
     # read the CE_DATA environment variable, which contains
     # the event data from the COS trigger
     event_data = os.getenv("CE_DATA")
