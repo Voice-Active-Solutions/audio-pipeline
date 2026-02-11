89b5e2d041ce65ea16a1d003ff1dc61a64012f50
James Page <james@ecreationtech.co.uk>
2026-02-11 00:45:51 +0000

    Added main function to main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
-from dotenv import load_dotenv 
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+import json
 import os
 
-load_dotenv() # This loads variables from .env into os.environ 
 
-bucket_name = os.getenv("BUCKET") 
+def main() -> None:
+    load_dotenv()
 
-print(f"Bucket name is: {bucket_name}")
+    bucket_name = os.getenv("BUCKET") 
+    print(f"Bucket name is: {bucket_name}")
 
-# read the CE_DATA environment variable, which contains the event data from the COS trigger
-event_data = os.getenv("CE_DATA")
+    # read the CE_DATA environment variable, which contains
+    # the event data from the COS trigger
+    event_data = os.getenv("CE_DATA")
 
-print(f"Event data is: {event_data}")
+    # extract the bucket name and object key from the event data as strings
+    if event_data is None:
+        print("No event data found in CE_DATA environment variable.")
+        return
+  
+    event_payload = json.loads(event_data)
+    bucket = event_payload.get("bucket")
+    object_key = event_payload.get("key")
+
+    print(f"Bucket is: {bucket}")
+    print(f"Object key is: {object_key}")
 
 
+if __name__ == "__main__":
+    main()
